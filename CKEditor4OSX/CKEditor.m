2319f6c52c2baedbcaedee351d24c762d0121fbf
Hermes Pique <hermes.pique@robotmedia.net>
2012-09-22 19:58:13 +0200

    Delete CKEditorTemplate

--- CKEditor4OSX/CKEditor.m
+++ CKEditor4OSX/CKEditor.m
@@ -10,7 +10,7 @@
 #import "CKUtils.h"
 #import "NSColor+String.h"
 
-NSString *kCKEditorTemplate = @"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><script type=\"text/javascript\" src=\"ckeditor.js\"></script></head><body><textarea id=\"editor\" name=\"editor\">&nbsp;</textarea><script type=\"text/javascript\"> CKEDITOR.replace('editor'); </script></body></html>";
+NSString *kCKEditorTemplate = @"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><script type=\"text/javascript\" src=\"ckeditor.js\"></script></head><body><textarea id=\"editor\" name=\"editor\">&nbsp;</textarea><script type=\"text/javascript\"> CKEDITOR.replace('editor', { on : { 'instanceReady': function(evt) { evt.editor.execCommand('maximize'); }}}); </script></body></html>";
 
 @implementation CKEditor
 
