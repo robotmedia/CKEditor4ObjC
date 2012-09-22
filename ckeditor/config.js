2c520442ebc3b9a5bca08572f08b45f2ec7fd1ce
Hermes Pique <hermes.pique@robotmedia.net>
2012-09-22 19:58:40 +0200

    OS X config

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,11 +1,21 @@
 ﻿/*
-Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
 CKEDITOR.editorConfig = function( config )
 {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    config.skin = 'osx';
+    config.extraPlugins = 'osxbuttons';
+    config.toolbarCanCollapse = false;
+    config.removePlugins = 'contextmenu, liststyle, tabletools, elementspath, resize'; // liststyle and tabletools are required to disable context menu. See: http://stackoverflow.com/a/12216307/143378
+    
+    config.toolbar = 'TextEdit';
+    
+    config.toolbar_TextEdit = [
+        { name: 'styles', items : [ 'OSXFont','FontSize' ] },
+        { name: 'font', items : [ 'OSXTextColor' ] },
+        { name: 'basicstyles', items : [ 'Bold', 'Italic', 'Underline' ] },
+        { name: 'paragraph', items : [ 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+    ];
 };
